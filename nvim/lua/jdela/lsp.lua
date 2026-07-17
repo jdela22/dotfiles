5e35a86035aa86f880eb90a160fdf8c348376d6d
Jared <jareddelapasse@gmail.com>
2026-07-16 21:37:48 -0500

    fix tmux nav in oil buffer

--- nvim/lua/jdela/lsp.lua
+++ nvim/lua/jdela/lsp.lua
@@ -41,7 +41,9 @@ vim.api.nvim_create_autocmd('LspAttach', {
       })
     end
 
-    vim.lsp.document_color.enable(false, { bufnr = event.buf })
+    -- Certain oklch colors were breaking the document_color feature of the css lsp
+    -- It seems to be working now (7/16/26) if still no problems, we can delete this
+    -- vim.lsp.document_color.enable(false, { bufnr = event.buf })
   end,
 })
 
@@ -85,9 +87,6 @@ require('mason-lspconfig').setup {
   },
 }
 
--- CSS LSP doesn't clamp conversion to rgb for high-chroma oklch colors
--- Nvim client crashes when trying to interpret the Out of bounds values
---
 -- Tailwind causes unknown @ rules warnings we also ignore
 vim.lsp.config.cssls = vim.tbl_deep_extend('force', vim.lsp.config.cssls or {}, {
   settings = {
@@ -98,8 +97,5 @@ vim.lsp.config.cssls = vim.tbl_deep_extend('force', vim.lsp.config.cssls or {}, 
       },
     },
   },
-  -- on_attach = function(client)
-  --   client.server_capabilities.colorProvider = false
-  -- end,
 })
 vim.lsp.enable 'cssls'
--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -4,11 +4,12 @@ return {
   config = function()
     require('oil').setup {
       columns = { 'icon' },
-      keymaps = { ['C-h'] = false, ['<M-h>'] = 'actions.select_split' },
+      keymaps = { ['<C-h>'] = vim.notify 'you pressed c-h', ['<M-h>'] = 'actions.select_split', ['<C-l>'] = false },
       view_options = { show_hidden = true },
     }
     vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent diretory' })
     vim.keymap.set('n', '<space>-', require('oil').toggle_float)
   end,
   dependencies = { { 'echasnovski/mini.icons', opts = {} } },
+  lazy = false,
 }
